65dc3c6303bac406cd12635c0f5a0e718553b7ea
taterg99 <taterg99@gmail.com>
2025-01-11 16:04:53 -0700

    Add project files.

---
+++ ClassesAssign2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoll_Assignment2
+{
+    internal class ClassesAssign2
+    {
+        // This method rolls the dice for how many times it is passed to it
+        public int[] RollDice(int iNumRolls)
+        {
+            // Use the Random class for later
+            Random rnd = new Random();
+
+            // Reset array to hold zeros for the combination of two dice rolled
+            int[] aiRollCount = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+            // Declare variable for each dice & Total
+            int iFirstDice;
+            int iSecondDice;
+            int iRollLocation;
+
+            // Loop through each roll for rolls passed
+            for (int iCount = 0; iCount < iNumRolls; iCount++)
+            {
+                // Roll both dice
+                iFirstDice = rnd.Next(1, 7);
+                iSecondDice = rnd.Next(1, 7);
+                // Add dice and subtract two for location
+                iRollLocation = iFirstDice + iSecondDice - 2;
+                // Add that roll to array (subtract 2 from roll number to get correct position)
+                aiRollCount[iRollLocation] += 1;
+            }
+
+            // Return the array
+            return aiRollCount;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using DiceRoll_Assignment2;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Declare array to hold rolls & a variable to hold roll count
+        int[] aiRollCount;
+        int iRolls;
+        // Variable to hold 
+        Console.WriteLine("Welcome to the dice throwing simulator!" + Environment.NewLine + Environment.NewLine + "How many dice rolls would you like to simulate?");
+        iRolls = int.Parse(Console.ReadLine());
+        
+        // Import the class
+        ClassesAssign2 cls = new ClassesAssign2();
+        // Get array from the RollDice method
+        aiRollCount = cls.RollDice(iRolls);
+        // Display the text before the asterisks
+        Console.WriteLine(Environment.NewLine + "DICE ROLLING SIMULATION RESULTS" + Environment.NewLine + "Each \"*\" represents 1% of the total number of rolls."
+            + Environment.NewLine + "Total number of rolls = " + iRolls + "." + Environment.NewLine + Environment.NewLine);
+        // Use method below to print out correct number of asterisks in a loop
+        for (int iCombination = 2; iCombination < 13; iCombination++)
+        {
+            Console.WriteLine($"{iCombination}: {GetAsterisks(aiRollCount[iCombination - 2], iRolls)}");
+        }
+
+        // Line break and end code with message
+        Console.WriteLine(Environment.NewLine + Environment.NewLine + "Thank you for using the dice throwing simulator. Goodbye!");
+
+    }
+
+    // This method returns a string of asterisks for the percent of the total that was sent as a parameter
+    private static string GetAsterisks(int iRolls, int iTotalRolls)
+    {
+        // Divide the iRolls by the iTotalRolls & times by 100 to get a percent and then round to integer
+        int iNumAsterisks = (int)Math.Round((double)iRolls / iTotalRolls * 100);
+        // Declare variable to hold asterisks
+        string sAsterisks = "";
+        // Loop through to add asterisks
+        for (int iCount = 0; iCount < iNumAsterisks; iCount++)
+        {
+            if (sAsterisks != "")
+            {
+                sAsterisks += "*";
+            } else
+            {
+                sAsterisks = "*";
+            }
+        }
+        // Return the asterisks
+        return sAsterisks;
+    }
+}
